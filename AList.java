f9805085f3b5e4c77dc81524037cd9f075af841f
soumyauduta <107532156+soumyauduta@users.noreply.github.com>
2022-08-15 12:07:16 +0530

    Add files via upload

---
+++ AList.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class AList {
+    public static void main(String[] args) {
+        ArrayList<Integer> li=new ArrayList<>();
+        li.add(15);
+        li.add(10);
+        li.add(11);
+        li.add(12);
+        li.add(13);
+        li.add(14);
+        System.out.println(li);
+    }
+}
